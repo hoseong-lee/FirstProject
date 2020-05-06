1132013794d91b41b6d376bccb55bab05dcc5224
Hoseong.Lee <ghtdjd2@naver.com>
2020-05-06 11:02:45 +0900

    Test.java Hello -> heo

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
-class Test{
-	public static void main(Stinrg [] args) {
-		System.out.println("Hello Git");
+class Test1{
+	public static void main(String [] args) {
+		System.out.println("Heo Git");
 		System.out.println("Hi Git");
-	}
+
+}
