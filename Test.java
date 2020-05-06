e91e5ce8f56d571c206683d2e96427365c48fb5d
Hoseong.Lee <ghtdjd2@naver.com>
2020-05-06 15:09:09 +0900

    test.java 2줄 주석

--- Test.java
+++ Test.java
@@ -1,8 +1,7 @@
 class Test{
 	public static void main(Stinrg [] args) {
 //		System.out.println("Hello");
-		System.out.println("Git");
-		System.out.println("Hi Git");
+//		System.out.println("Git");
 		System.out.println("branch1");
 		System.out.println("branch2");
 	}
