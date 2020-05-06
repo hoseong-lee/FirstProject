e0a80505e0f0896ba4045ef725ae26e34303dfd2
Hoseong.Lee <ghtdjd2@naver.com>
2020-05-06 15:01:59 +0900

    test.java 첫 줄 주석

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(Stinrg [] args) {
-		System.out.println("Hello");
+//		System.out.println("Hello");
 		System.out.println("Git");
 		System.out.println("Hi Git");
 		System.out.println("branch1");
